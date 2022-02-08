6506fe9ed036cd2c8aab2e8b9c40d4d5dd61bcdb
gustavogmail <gustavograal@gmail.com>
2022-02-08 00:32:59 -0300

    Adding todo project

---
+++ todo/precache-manifest.434848b338e3ae8476b990fc8396e089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20127e4d3efc488d65146da0cc7c2d27",
+    "url": "/alexdevero/react-hooks-todo-list-app-ts/index.html"
+  },
+  {
+    "revision": "a7b8ef05035a8dc7eef1",
+    "url": "/alexdevero/react-hooks-todo-list-app-ts/static/css/main.14ea48b3.chunk.css"
+  },
+  {
+    "revision": "f90261cfd89d614b5b33",
+    "url": "/alexdevero/react-hooks-todo-list-app-ts/static/js/2.2daf5f13.chunk.js"
+  },
+  {
+    "revision": "a7b8ef05035a8dc7eef1",
+    "url": "/alexdevero/react-hooks-todo-list-app-ts/static/js/main.fb881db1.chunk.js"
+  },
+  {
+    "revision": "84a9937b183625c73b4b",
+    "url": "/alexdevero/react-hooks-todo-list-app-ts/static/js/runtime-main.73220504.js"
+  }
+]);
